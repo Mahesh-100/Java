124f6bc5c047cfcf454680595d6b5eb6fe4b64a8
Mahesh-100 <maheshkumar.ucen@gmail.com>
2023-12-24 21:31:38 +0530

    Demo

---
+++ practice/Demo.java
@@ -0,0 +1,13 @@
+package com.practice;
+public class Demo {
+
+	public static void main(String[] args) {
+		BinaryConvertor bn= new BinaryConvertor();
+        bn.binaryConvertor(32);	
+        SumDigits sd= new SumDigits();
+        System.out.println("the sum of digits:" +sd.sumdigits(345));    
+	
+	}
+
+}
+	
