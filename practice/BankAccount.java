fac6c7d2cbef35ea870e93e126cee9b5f2f0bcb1
Mahesh-100 <maheshkumar.ucen@gmail.com>
2023-12-25 23:37:11 +0530

    Encapsulation

---
+++ practice/BankAccount.java
@@ -0,0 +1,19 @@
+package com.practice;
+
+public class BankAccount {
+	private String AccountNumber;
+	private String AccounBalance;
+	public String getAccountNumber() {
+		return AccountNumber;
+	}
+	public void setAccountNumber(String accountNumber) {
+		AccountNumber = accountNumber;
+	}
+	public String getAccounBalance() {
+		return AccounBalance;
+	}
+	public void setAccounBalance(String accounBalance) {
+		AccounBalance = accounBalance;
+	}
+
+}
--- practice/Demo.java
+++ practice/Demo.java
@@ -10,12 +10,28 @@ public class Demo {
         System.out.println("The smallest number is:"+sm.smallestnumber(2,99,14));
         VowelsCount VC=new VowelsCount();
         System.out.println("The number of vowels are:"+VC.vowels_count("the iiqqee"));
-        Quadrilateral eq=new Quadrilateral();
-        eq.solveEquation(1,-2,1);
+        Quadratic eq=new Quadratic();
+        eq.solveEquation(1, 2, 1);
         Table t=new Table();
         t.table(4);
-        
-	}
+        BankAccount b=new BankAccount();
+        b.setAccountNumber("ACC001");
+        b.setAccounBalance("899900");
+        System.out.println("the bank account number:"+b.getAccountNumber()+" with Balance :"+b.getAccounBalance());
+        Circle circle=new Circle(7);
+    	System.out.println("Area of circle:"+circle.calculateArea());
+    	Rectangle rectangle=new Rectangle(4,6);
+    	System.out.println("Area of rectangle:"+rectangle.calculateArea());
+
+    	Triangle triangle=new Triangle(5,10);
+    	System.out.println("Area of triangle:"+triangle.calculateArea());
+
+	
+	
+	};
+	
+		
+		
+		
 
 }
-	
