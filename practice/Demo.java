b6e2e4a60c08ebe4cfd8ca406cc26d0b3a6e33dd
Mahesh-100 <maheshkumar.ucen@gmail.com>
2023-12-25 13:18:36 +0530

    updated Demo

--- practice/Demo.java
+++ practice/Demo.java
@@ -5,8 +5,16 @@ public class Demo {
 		BinaryConvertor bn= new BinaryConvertor();
         bn.binaryConvertor(32);	
         SumDigits sd= new SumDigits();
-        System.out.println("the sum of digits:" +sd.sumdigits(345));    
-	
+        SmallestNumber sm= new SmallestNumber();
+        System.out.println("the sum of digits:" +sd.sumdigits(345));
+        System.out.println("The smallest number is:"+sm.smallestnumber(2,99,14));
+        VowelsCount VC=new VowelsCount();
+        System.out.println("The number of vowels are:"+VC.vowels_count("the iiqqee"));
+        Quadrilateral eq=new Quadrilateral();
+        eq.solveEquation(1,-2,1);
+        Table t=new Table();
+        t.table(4);
+        
 	}
 
 }
