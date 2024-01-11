173e7f5ab984b76a74f9e016b5d0dbae590d3bba
Mahesh-100 <maheshkumar.ucen@gmail.com>
2024-01-11 22:03:40 +0530

    DAO

---
+++ DAO/StudentDAOImpl.java
@@ -0,0 +1,38 @@
+package DAO;
+
+import java.sql.SQLException;
+import java.util.List;
+
+public class StudentDAOImpl implements StudentDAO{
+
+	public Student get(int id) throws SQLException {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	public List<Student> getAll() throws SQLException {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	public int save(Student tstudent) throws SQLException {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	public int insert(Student tstudent) throws SQLException {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	public int update(Student student) throws SQLException {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	public int delete(Student student) {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+}
